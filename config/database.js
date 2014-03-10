50bc69584a9d2ef8084d84e79869d0b77efa526e
James Donovan <james@donovanjames.com>
2014-03-10 01:37:31 -0400

    added random images selection, neDB support, upadted routes and views.

---
+++ config/database.js
@@ -0,0 +1,29 @@
+// Require nedb module
+var dataStore = require('nedb'),
+	fs = require('fs');
+
+// create nedb for photos and users leveraging autoload
+var images = new dataStore({ filename: __dirname + "/data/images", autoload: true }),
+	users = new dataStore({ filename: __dirname + "/data/users", autoload: true});
+
+//  Create unique filename for photos and user ip
+images.ensureIndex({fieldName: 'name', unique: true});
+users.ensureIndex({fieldName: 'ip', unique: true});
+
+// Read filesystem photos into nedb objects
+var  defaultImages = fs.readdirSync('./public/images/starter');
+
+//
+defaultImages.forEach(function(image){
+	images.insert({
+		name: image,
+		wins: 0,
+		loses: 0
+	})
+});
+
+// Make users and images universally available
+module.exports = {
+	images:images,
+	users:users
+};
--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,13 @@ module.exports = function(app){
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
 
-    var photos = require('../app/controllers/photos')
-    app.get('/photos', photos.get )
+	var photos = require('../app/controllers/photos');
+	app.get('/photos', photos.get );
+
+	var vote = require('../app/controllers/vote');
+	app.get('/vote', vote.show);
+
+	var list = require('../app/controllers/scope');
+	app.get('/scope', list.scope);
 
 };
