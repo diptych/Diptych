0139f91e327a788821e5304dea75689ac2d13d86
Evan Schneider <evanrs@gmail.com>
2014-04-05 19:58:47 -0400

    Implemented keep, toss, pair endpoints. Added Q library.

--- config/database.js
+++ config/database.js
@@ -3,22 +3,27 @@ var dataStore = require('nedb'),
 	fs = require('fs');
 
 // create nedb for photos and users leveraging autoload
-var images = new dataStore({ filename: __dirname + "/data/images", autoload: true }),
-	users = new dataStore({ filename: __dirname + "/data/users", autoload: true});
+var images = new dataStore({ filename: __dirname + "/data/images", autoload: true })
+,   users = new dataStore({ filename: __dirname + "/data/users", autoload: true})
+;
+
 
 //  Create unique filename for photos and user ip
 images.ensureIndex({fieldName: 'name', unique: true});
 users.ensureIndex({fieldName: 'ip', unique: true});
 
+
 // Read filesystem photos into nedb objects
 var  defaultImages = fs.readdirSync('./public/img/starter');
 
-//
 defaultImages.forEach(function(image){
 	images.insert({
 		name: image,
-		wins: 0,
-		loses: 0
+        wins: [],
+        losses: [],
+        pairs: [],
+        tosses: []
+
 	})
 });
 
